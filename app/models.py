47588850de327ac58b46b38392af268e7e71e7a5
Chimdinyelum <chyjohn777@gmail.com>
2025-11-30 23:35:30 +0100

    feat: Added models for the project

--- app/models.py
+++ app/models.py
@@ -1,4 +1,8 @@
+from django.conf import settings
+from django.contrib import admin
+from django.core.validators import MinValueValidator
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
 
@@ -10,16 +14,21 @@ class Promotion(models.Model):
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+    featured_product_id = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_updated = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
+    slug = models.CharField(max_length=255, default='-')
 
     def __str__(self):
         return self.title
@@ -37,19 +46,26 @@ class Customer(models.Model):
         (M_SILVER, 'Silver'),
         (M_GOLD, 'Gold'),
     ]
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField(max_length=100, unique=True)
     phone = models.CharField(max_length=20)
-    birth_date = models.DateField(null=True)
+    birth_date = models.DateField(null=True, blank=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_STATUS, default=M_BRONZE)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.first_name
+        return self.user.first_name
+
+    @admin.display(ordering='user__first_name')
+    def first_name(self):
+        return self.user.first_name
+
+    @admin.display(ordering='user__last_name')
+    def last_name(self):
+        return self.user.last_name
 
     class Meta:
-        ordering = ['first_name']
+        ordering = ['user__first_name', 'user__last_name']
 
 
 class Order(models.Model):
@@ -82,9 +98,22 @@ class Address(models.Model):
 
 class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
+    cart_id = models.UUIDField(unique=True, default=uuid4, editable=False)
 
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveSmallIntegerField()
+    quantity = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1)]
+    )
+
+    class Meta:
+        unique_together = [['cart', 'product']]
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateTimeField(auto_now=True)
