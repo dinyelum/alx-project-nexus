0af23f532126d8d7229328f005fbe5bf04d7ca0c
Chimdinyelum <chyjohn777@gmail.com>
2025-12-01 00:20:01 +0100

    feat: Added urls and settings for swagger

--- storefront/urls.py
+++ storefront/urls.py
@@ -17,15 +17,42 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import settings
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your API",
+        default_version='v1',
+        description="API description",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 admin.AdminSite.site_header = 'Storefront Admin'
 admin.AdminSite.index_title = 'Admin'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('playground/', include('playground.urls'))
+    path('playground/', include('playground.urls')),
+    path('store/', include('app.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('swagger/', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
 
 ]
+"""
+Remember; 
+if the url starts with admin, it should be handled by admin.site.urls
+if the url starts with playground, it should be handled by playground.urls
+if the url starts with store, it should be handled by app.urls
+
+"""
 
 if settings.DEBUG:
     import debug_toolbar
