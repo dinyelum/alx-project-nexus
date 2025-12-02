900defce7c77e785dca208d6b674e2a1337cfece
Chimdinyelum <chyjohn777@gmail.com>
2025-12-02 12:37:41 +0100

    Feat: added Profile

--- storefront/urls.py
+++ storefront/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import settings
+from django.conf import settings
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
