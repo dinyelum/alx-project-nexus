a057ee07050a6b060675d7c386f85bf0f41b50da
Chimdinyelum <chyjohn777@gmail.com>
2025-12-03 10:54:52 +0100

    feat: sql for product and collection

--- app/management/commands/seed.sql
+++ app/management/commands/seed.sql
@@ -1,16 +1,16 @@
--- insert into
---   app_collection (id, title, featured_product_id)
--- values
---   (1, 'Flowers', null),
---   (2, 'Grocery', null),
---   (3, 'Beauty', null),
---   (4, 'Cleaning', null),
---   (5, 'Stationary', null),
---   (6, 'Pets', null),
---   (7, 'Baking', null),
---   (8, 'Spices', null),
---   (9, 'Toys', null),
---   (10, 'Magazines', null);
+insert into
+  app_collection (id, title, featured_product_id)
+values
+  (1, 'Flowers', null),
+  (2, 'Grocery', null),
+  (3, 'Beauty', null),
+  (4, 'Cleaning', null),
+  (5, 'Stationary', null),
+  (6, 'Pets', null),
+  (7, 'Baking', null),
+  (8, 'Spices', null),
+  (9, 'Toys', null),
+  (10, 'Magazines', null);
 
 insert into
   app_product (
