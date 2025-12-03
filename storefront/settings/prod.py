af62aec196eee7f9a472d168879143cb3823f27b
Chimdinyelum <chyjohn777@gmail.com>
2025-12-03 06:23:17 +0100

    chore: add SQL strict mode to database config

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -8,5 +8,10 @@ DEBUG = False
 ALLOWED_HOSTS = ["joestores-prod-1d75a0612d5e.herokuapp.com"]
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        **dj_database_url.config(),  # Unpack the database config
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    }
 }
